f4d61f78386fa98afb16eaa17ff0268a0a2be6af
anton <tony.potapog@gmail.com>
2018-06-17 22:24:57 +0300

    refactor play method

--- lib/game.rb
+++ lib/game.rb
@@ -6,18 +6,25 @@ require_relative 'combination'
 class Game
   def play
     @deck = CardStack.card_deck
+    hand_over
+    flop_turn_river
+    # Player A win: 2D 4D 7D KD AD (flush)
+    puts determin_winner
+  end
 
+  def hand_over
     # Player A hand: AD KD
-    hand_a = CardStack.hand(@deck)
-    puts "Player A hand #{hand_a.cards}"
-    # deck.cards = deck.cards - hand_a.cards
+    @hand_a = CardStack.hand(@deck)
+    puts "Player A hand #{@hand_a.cards}"
     # Player B hand: AC TD
-    hand_b = CardStack.hand(@deck)
-    puts "Player B hand #{hand_b.cards}"
+    @hand_b = CardStack.hand(@deck)
+    puts "Player B hand #{@hand_b.cards}"
     # Player C hand: QD QC
-    hand_c = CardStack.hand(@deck)
-    puts "Player C hand #{hand_c.cards}"
+    @hand_c = CardStack.hand(@deck)
+    puts "Player C hand #{@hand_c.cards}"
+  end
 
+  def flop_turn_river
     # Table on flop: 2D 5C 7D
     @table = CardStack.table_on_flop(@deck)
     puts "table on flop #{@table.cards}"
@@ -28,15 +35,12 @@ class Game
     CardStack.add_card_to_table(@table, @deck)
     puts "table on river #{@table.cards}"
     puts "cards in deck left #{@deck.cards.count}"
-
-    # Player A win: 2D 4D 7D KD AD (flush)
-    puts determin_winner(hand_a, hand_b, hand_c)
   end
 
-  def determin_winner(hand_a, hand_b, hand_c)
-    combination1 = Combination.new(hand_a, @table)
-    combination2 = Combination.new(hand_b, @table)
-    combination3 = Combination.new(hand_c, @table)
+  def determin_winner#(hand_a, hand_b, hand_c)
+    combination1 = Combination.new(@hand_a, @table)
+    combination2 = Combination.new(@hand_b, @table)
+    combination3 = Combination.new(@hand_c, @table)
 
     hand = Combination.highest_hand(combination1, combination2, combination3)
 
